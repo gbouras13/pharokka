5748c7ff621a3ffa0115796f0c9b61334981a256
gbouras13 <george.bouras@adelaide.edu.au>
2021-12-09 16:53:05 +1030

    phanotate and translate

--- modules/input_commands.py
+++ modules/input_commands.py
@@ -2,8 +2,6 @@ import argparse
 import os
 import sys
 from argparse import RawTextHelpFormatter
-from Bio import SeqIO
-
 
 ROOT_DIR = os.path.dirname(os.path.abspath("VERSION"))
 version_file = open(os.path.join(ROOT_DIR, 'VERSION'))
@@ -12,15 +10,15 @@ v = version_file.read().strip()
 def get_input():
 	usage = 'phrokka ...'
 	parser = argparse.ArgumentParser(description='phrokka: phage genome annotation piepline', formatter_class=RawTextHelpFormatter)
-	parser.add_argument('-i', '--infile', help='input file in fasta format', type=argparse.FileType('r'), required=True)
+	parser.add_argument( '-i', '--infile', action="store", help='input file in fasta format',  required=True)
 	parser.add_argument('-o', '--outdir', action="store", help='where to write the output [stdout]', default=sys.stdout)
 	parser.add_argument('-V', '--version', action='version', version=v)
 	args = parser.parse_args()
 
 	return args
 
+def instantiate_outdir():
+	out_dir = "output/"
+	if os.path.isdir(out_dir) == False:
+		os.mkdir(out_dir)
 
-def read_fasta(filepath):
-
-	for record in SeqIO.parse(filepath, "fasta"):
-	print(record.id)
