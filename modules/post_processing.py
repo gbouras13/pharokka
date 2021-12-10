dafd661853b489698d472cd34ced7fed1c43bcba
gbouras13 <georgebouras@Georges-MBP.home.gateway>
2021-12-10 22:56:07 +1030

    get top mmseqs hits

--- modules/post_processing.py
+++ modules/post_processing.py
@@ -4,3 +4,23 @@ import subprocess as sp
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import pandas as pd
+
+def process_mmseqs_results():
+    input_dir = "output/"
+    mmseqs_file = input_dir + "mmseqs_results.tsv"
+    print("Processing mmseqs output")
+    col_list = ["phrog", "gene", "alnScore", "seqIdentity", "eVal", "qStart", "qEnd", "qLen", "tStart", "tEnd", "tLen"] 
+    mmseqs_df = pd.read_csv(mmseqs_file, delimiter= '\t', index_col=False , names=col_list) 
+    genes = mmseqs_df.gene.unique()
+    print(genes)
+
+    tophits = []
+
+    for gene in genes:
+        tmp_df = mmseqs_df.loc[mmseqs_df['gene'] == gene].sort_values('eVal').reset_index(drop=True).loc[0]
+        tophits.append([tmp_df.phrog, tmp_df.gene, tmp_df.alnScore, tmp_df.seqIdentity, tmp_df.eVal])
+        print(tophits)
+
+    tophits_df = pd.DataFrame(tophits, columns=['phrog', 'gene', 'alnScore', 'seqIdentity', 'eVal'])
+    print(tophits_df)
+    tophits_df.to_csv(input_dir + "top_hits.tsv", sep="\t")
