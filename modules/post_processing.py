6bf7add52be7234202505f4d195b9a9749674433
gbouras13 <georgebouras@Georges-MBP.home.gateway>
2021-12-13 00:30:54 +1030

    merge phrogs

--- modules/post_processing.py
+++ modules/post_processing.py
@@ -6,8 +6,7 @@ from Bio.SeqRecord import SeqRecord
 import pandas as pd
 
 def process_mmseqs_results():
-    input_dir = "output/"
-    mmseqs_file = input_dir + "mmseqs_results.tsv"
+    mmseqs_file =  "output/mmseqs_results.tsv"
     print("Processing mmseqs output")
     col_list = ["phrog", "gene", "alnScore", "seqIdentity", "eVal", "qStart", "qEnd", "qLen", "tStart", "tEnd", "tLen"] 
     mmseqs_df = pd.read_csv(mmseqs_file, delimiter= '\t', index_col=False , names=col_list) 
@@ -22,16 +21,28 @@ def process_mmseqs_results():
         print(tophits)
 
     tophits_df = pd.DataFrame(tophits, columns=['phrog', 'gene', 'alnScore', 'seqIdentity', 'eVal'])
-    tophits_df.to_csv(input_dir + "top_hits.tsv", sep="\t")
-    # left join 
-    input_dir = "output/"
-    phan_file = input_dir + "cleaned_phanotate.tsv"
-    col_list = ["ind" ,"start", "stop", "frame", "contig", "score", "gene"] 
-    phan_df = pd.read_csv(phan_file, sep="\t", index_col=False, names=col_list)
+    tophits_df.to_csv("output/top_hits.tsv", sep="\t", index=False)
+    # left join mmseqs top hits to phanotate
+    phan_file = "output/cleaned_phanotate.tsv"
+    # automatically picks up the names
+    #col_list = ["start", "stop", "frame", "contig", "score", "gene"] 
+    phan_df = pd.read_csv(phan_file, sep="\t", index_col=False )
     phan_df['gene']=phan_df['gene'].astype(str)
     tophits_df['gene']=tophits_df['gene'].astype(str)
+    # merge top hit
     merged_df = phan_df.merge(tophits_df, on='gene', how='left')
+    merged_df[['phrog','top_hit']] = merged_df['phrog'].str.split(' ## ',expand=True)
+    merged_df["phrog"] = merged_df["phrog"].str.replace("phrog_", "")
+    # get phrog annotaion file
+    phrog_annot_df = pd.read_csv('databases/phrog_annot_v3.tsv', sep="\t", index_col=False )
+    # merge phrog
+    phrog_annot_df['phrog']=phrog_annot_df['phrog'].astype(str)
+    merged_df = merged_df.merge(phrog_annot_df, on='phrog', how='left')
+
+    merged_df.to_csv("output/final_merged_output.tsv", sep="\t", index=False)
     print(merged_df)
+
+
 
 
 
