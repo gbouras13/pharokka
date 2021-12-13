459041cf864f6539cf8a65d099e7d8fd92d2945c
gbouras13 <george.bouras@adelaide.edu.au>
2021-12-13 15:17:36 +1030

    gff and tbl

--- phrokka.py
+++ phrokka.py
@@ -9,10 +9,13 @@ from modules import post_processing
 if __name__ == "__main__":
     args = input_commands.get_input()
     #processes.run_phanotate(args.infile)
-    processes.translate_fastas()
+    #processes.translate_fastas()
     #processes.run_trna_scan(args.infile)
     processes.run_mmseqs()
-    post_processing.process_mmseqs_results()
+    phan_mmseq_merge_df = post_processing.process_mmseqs_results()
+    length_df = post_processing.get_contig_name_lengths(args.infile)
+    post_processing.create_gff(phan_mmseq_merge_df, length_df)
+    post_processing.create_tbl(phan_mmseq_merge_df, length_df)
     sys.exit("phrokka has finished")  # pragma: no cover
 
 
