817434087ed8bbeea0c4906be7f601641d2ef7ab
gbouras13 <george.bouras@adelaide.edu.au>
2022-07-18 01:48:18 +0930

    change db flag

--- bin/install_databases.py
+++ bin/install_databases.py
@@ -13,7 +13,7 @@ from argparse import RawTextHelpFormatter
 def get_db_input():
 	usage = 'install_databases.py -d N -o "path/to/dir'
 	parser = argparse.ArgumentParser(description='script to download required phrog databases', formatter_class=RawTextHelpFormatter)
-	parser.add_argument('-d', '--default', action="store", help='Must be "Y" or "N". Determines whether you want databases stored in the default location, or in a custom directory',
+	parser.add_argument('-d', '--default', action="store_true" , help='Determines whether you want databases stored in the default location, or in a custom directory',
     required=True)
 	parser.add_argument('-o', '--outdir', action="store", help='Database Directory - will be created and must be specificed in conjunction with -d N')
 	args = parser.parse_args()
@@ -23,17 +23,14 @@ def get_db_input():
 args = get_db_input()
 print(args.outdir)
 
-if args.default == 'Y':
+if args.default == True:
     db_dir = os.path.join(os.path.dirname(__file__),'../',"databases/")  
     databases.instantiate_install(db_dir)
-elif args.default == 'N':
+else:
     if args.outdir == None:
-        print("-o was not specified, but -d N was.")
-        print("Downloading databases to the default directory anyway")
+        print("--outdir was not specified.")
+        print("Downloading databases to the default directory anyway.")
         db_dir = os.path.join(os.path.dirname(__file__),'../',"databases/")  
     else:
         db_dir = args.outdir
     databases.instantiate_install(db_dir)
-else:
-    print("-d was not specified as Y or N. Please try again.")
-    exit()
